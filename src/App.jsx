aa1f49c495207c0d4ed9876bdc06fd0a2c8b7b1f
saqibbedar <chandio.saqib.m@gmail.com>
2024-10-16 12:11:19 +0500

    feat: add react-toastify for notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { Route, Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Login, Chat, ProfileUpdate } from "./pages/page";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <>
+      <ToastContainer theme="dark"/>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/chat" element={<Chat />} />
